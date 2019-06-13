9c1eb93634d9bc914c2884f1d5683d044e2baded
echan-sage <eric.chan@sage.com>
2019-06-13 16:05:36 -0700

    missing commits

---
+++ FormExtractor/FormExtractor/Models/ExtractViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormExtractor.Models
+{
+    public class ExtractViewModel
+    {
+        public ExtractViewModel()
+        {
+            Vendors = new List<Vendor>();
+        }
+
+        public List<Vendor> Vendors { get; set; }
+    }
+}
