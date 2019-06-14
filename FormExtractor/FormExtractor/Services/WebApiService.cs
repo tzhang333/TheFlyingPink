c89b50035910841c1f5005055d610d7c07750171
echan-sage <eric.chan@sage.com>
2019-06-14 11:57:30 -0700

    Add GET vendors with webapi

--- FormExtractor/FormExtractor/Services/WebApiService.cs
+++ FormExtractor/FormExtractor/Services/WebApiService.cs
@@ -1,23 +1,31 @@
 ﻿using FormExtractor.Models;
+using Newtonsoft.Json;
 using RestSharp;
+using RestSharp.Authenticators;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Script.Serialization;
 
 namespace FormExtractor.Services
 {
     public class WebApiService
     {
         RestClient _client;
+        string _baseUrl;
 
         public WebApiService()
         {
-            var url = ConfigurationManager.AppSettings["Sage300WebApiUrl"];
-            _client = new RestClient(url);
+            _baseUrl = ConfigurationManager.AppSettings["Sage300WebApiUrl"];
+            var user = ConfigurationManager.AppSettings["WebApiUser"];
+            var pass = ConfigurationManager.AppSettings["WebApiPass"];
+            _client = new RestClient(_baseUrl);
+            _client.Authenticator = new HttpBasicAuthenticator(user, pass);
         }
 
         private static Vendor MapVendor(SageVendor sageVendor)
@@ -38,17 +46,27 @@ namespace FormExtractor.Services
             };
         }
 
-        public async Task<List<Vendor>> GetVendors(string company)
+        public List<Vendor> GetVendors(string company)
         {
             var list = new List<Vendor>();
-            var request = new RestRequest(string.Format("{0}/AP/APVendors", company), Method.GET);
-            var response = await _client.ExecuteGetTaskAsync<List<SageVendor>>(request);
 
-            if(response.StatusCode == HttpStatusCode.OK)
+            var url = string.Format("{0}{1}/AP/APVendors", _baseUrl, company);
+            var request = new RestRequest(url, Method.GET);
+
+            var response = _client.Get<dynamic>(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
             {
-                foreach(var data in response.Data)
+                var data = (Dictionary<string, object>)response.Data;
+                object sdata = null;
+                data.TryGetValue("value", out sdata);
+
+                var serializer = new JsonSerializer();
+                var ldata = JsonConvert.DeserializeObject<List<SageVendor>>(sdata.ToString());
+
+                foreach (var vendor in ldata)
                 {
-                    list.Add(MapVendor(data));
+                    list.Add(MapVendor(vendor));
                 }
             }
 
