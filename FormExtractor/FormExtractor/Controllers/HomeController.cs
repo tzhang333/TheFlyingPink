fc975cc7f1a3d523c9e157b8217c9421c800a84f
tzhang333 <tian.zhang@sage.com>
2019-06-14 13:14:37 -0700

    Integrate with .net call

--- FormExtractor/FormExtractor/Controllers/HomeController.cs
+++ FormExtractor/FormExtractor/Controllers/HomeController.cs
@@ -14,12 +14,15 @@ using FormExtractor.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using WebGrease.Css.Extensions;
+using ACCPAC.Advantage;
 
 namespace FormExtractor.Controllers
 {
     public class HomeController : Controller
     {
         private WebApiService _service;
+        private ACCPAC.Advantage.Session AccpacSession;
+        private ACCPAC.Advantage.DBLink dblink;
 
         public HomeController()
         {
@@ -92,6 +95,11 @@ namespace FormExtractor.Controllers
                     var jo = JObject.Parse(JSON);
                     var invNo = jo["pages"][0]["keyValuePairs"][1]["value"][0]["text"];
 
+                    /// Insert into Sage 300
+                    OpenSession("SAMINC", "ADMIN", "ADMIN");
+                    var apInvsrc = ParseForminJSON(JSON);
+                    createAPBatch(apInvsrc.invoiceNo, "1200", apInvsrc.amounts);
+
                     var invoice = new AzureInvoice()
                     {
                         Id = invNo.ToString(),
@@ -115,5 +123,153 @@ namespace FormExtractor.Controllers
                 successCount,
             });
         }
+        private APInvSrc ParseForminJSON(String JSON)
+        {
+            APInvSrc apInv = new APInvSrc();
+            var jo = JObject.Parse(JSON);
+            apInv.invoiceNo = jo["pages"][0]["keyValuePairs"][1]["value"][0]["text"].ToString();
+            List<String> amounts = new List<String>();
+            var am = jo["pages"][0]["tables"][1]["columns"][5]["entries"].ToArray();
+            foreach (var a in am)
+            {
+                var abc = a[0]["text"];
+                if (abc != null)
+                {
+                    amounts.Add(abc.ToString());
+                }
+            }
+            apInv.amounts = amounts;
+
+            return apInv;
+        }
+        private int OpenSession(string sOrgID, string sUserID, string sPassword)
+        {
+            AccpacSession = new ACCPAC.Advantage.Session();
+            try
+            {
+                AccpacSession.Init("", "XZ", "XZ1000", "66A");
+                DateTime dt = DateTime.Today;
+                AccpacSession.Open(sUserID, sPassword, sOrgID, dt, 0);
+            }
+            catch (Exception ex)
+            {
+                Error("Exception:" + ex.ToString());
+                return 1;
+            }
+
+            dblink = AccpacSession.OpenDBLink(DBLinkType.Company, DBLinkFlags.ReadWrite);
+            return 0;
+        }
+        private int createAPBatch(string invNo, string vendorNo, List<string> amounts)
+        {
+            View AP0020 = OpenView("AP0020"); //Invoice Batches
+            View AP0021 = OpenView("AP0021"); //Invoice
+            View AP0022 = OpenView("AP0022"); //Invoice Details
+            View AP0023 = OpenView("AP0023"); //Payment Schedule
+            View AP0402 = OpenView("AP0402"); //Invoice Optional Fields
+            View AP0401 = OpenView("AP0401"); //Invoice Detail Optional field
+
+            View[] v1 = new View[1];
+            v1[0] = AP0021;
+            AP0020.Compose(v1);
+
+            View[] v2 = new View[4];
+            v2[0] = AP0020;
+            v2[1] = AP0022;
+            v2[2] = AP0023;
+            v2[3] = AP0402;
+            AP0021.Compose(v2);
+
+            View[] v3 = new View[3];
+            v3[0] = AP0021;
+            v3[1] = AP0020;
+            v3[2] = AP0401;
+            AP0022.Compose(v3);
+
+            View[] v4 = new View[1];
+            v4[0] = AP0021;
+            AP0023.Compose(v4);
+
+            View[] v5 = new View[1];
+            v5[0] = AP0021;
+            AP0402.Compose(v5);
+
+            View[] v6 = new View[1];
+            v6[0] = AP0022;
+            AP0401.Compose(v6);
+
+            AP0020.RecordCreate(ViewRecordCreate.Insert);
+            AP0020.Fields.FieldByName("PROCESSCMD").SetValue(1, false);
+            AP0020.Process();
+
+            AP0020.Read(false);
+            AP0021.RecordCreate(ViewRecordCreate.NoInsert);
+            AP0022.Cancel();
+            AP0021.Fields.FieldByName("IDVEND").SetValue(vendorNo, false);
+            AP0021.Fields.FieldByName("PROCESSCMD").SetValue(7, false);
+            AP0021.Process();
+
+            AP0021.Fields.FieldByName("PROCESSCMD").SetValue(7, false);
+            AP0021.Process();
+
+            AP0021.Fields.FieldByName("PROCESSCMD").SetValue(4, false);
+            AP0021.Process();
+
+            AP0022.Fields.FieldByName("CNTLINE").SetValue(-1, false);
+            AP0022.Read(false);
+            AP0022.Delete();
+            //AP0022.Fields.FieldByName("AMTDIST").SetValue(777.55, false);
+            //AP0022.Update();
+
+            //AP0022.Fields.FieldByName("CNTLINE").SetValue(-1, false);
+            //AP0022.Read(false);
+            foreach (var amount in amounts)
+            {
+                AP0022.RecordCreate(ViewRecordCreate.DelayKey);
+                AP0022.Fields.FieldByName("PROCESSCMD").SetValue(0, false);
+                AP0022.Process();
+                AP0022.Fields.FieldByName("IDDIST").SetValue("AMEX", false);
+                AP0022.Fields.FieldByName("AMTDIST").SetValue(amount, false);
+                AP0022.Insert();
+            }
+
+            Decimal distAmount = (Decimal)AP0021.Fields.FieldByName("AMTUNDISTR").Value;
+            AP0021.Fields.FieldByName("AMTGROSTOT").SetValue(Decimal.Negate(distAmount), false);
+            AP0021.Fields.FieldByName("IDINVC").SetValue(invNo, false);
+            AP0021.Insert();
+
+            AP0021.Fields.FieldByName("PROCESSCMD").SetValue(7, false);
+            AP0021.Process();
+            AP0020.Read(false);
+
+            return 0;
+        }
+
+        private View OpenView(string sViewID)
+        {
+            ACCPAC.Advantage.View v;
+            try
+            {
+                v = dblink.OpenView(sViewID);
+            }
+            catch (Exception ex)
+            {
+                Error("Exception:" + ex.ToString());
+                return null;
+            }
+            if (v == null) return null;
+            return v;
+        }
+
+        private void Error(string sText)
+        {
+            Console.WriteLine("Error:" + sText);
+        }
+    }
+
+    class APInvSrc
+    {
+        public String invoiceNo { get; set; }
+        public List<String> amounts { get; set; }
     }
 }
