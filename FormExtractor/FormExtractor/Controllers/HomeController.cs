53654af890ef554fb4e44f3b6e939fd7ce3bd6c5
echan-sage <eric.chan@sage.com>
2019-06-13 17:29:05 -0700

    Display uploaded invoice number to user (gotten from Tian). parser may change later

--- FormExtractor/FormExtractor/Controllers/HomeController.cs
+++ FormExtractor/FormExtractor/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using FormExtractor.Models;
 using FormExtractor.Services;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -38,20 +40,52 @@ namespace FormExtractor.Controllers
         [HttpPost]
         public async Task<ActionResult> FileUpload(IEnumerable<HttpPostedFileBase> files)
         {
+            var invoices = new List<AzureInvoice>();
+            var successCount = 0;
             if (files != null)
             {
-                var tasks = new List<Task>();
+                var tasks = new List<Task<HttpResponseMessage>>();
                 foreach (HttpPostedFileBase file in files)
                 {
+                    // upload files to server
                     FileService.Upload(file);
 
+                    // upload files from server to Azure
                     tasks.Add(AzureService.PostFormRecognizer(file.FileName));
                 }
 
                 await Task.WhenAll(tasks);
+                foreach(var task in tasks)
+                {
+                    var response = task.Result;
+                    var success = (response.StatusCode == System.Net.HttpStatusCode.OK) ? true : false;
+                    var JSON = await response.Content.ReadAsStringAsync();
+
+                    var jo = JObject.Parse(JSON);
+                    var invNo = jo["pages"][0]["keyValuePairs"][1]["value"][0]["text"];
+
+                    var invoice = new AzureInvoice()
+                    {
+                        Id = invNo.ToString(),
+                        Total = 0,
+                        Success = success,
+                        JSON = JSON,
+                    };
+                    invoices.Add(invoice);
+
+                    if (success)
+                    {
+                        successCount++;
+                    }
+                }
             }
 
-            return RedirectToAction("Extract", new { type = "Invoice" });
+            return Json(new
+            {
+                invoices,
+                redirectUrl = Url.Action("Extract", new { type = "Invoice" }),
+                successCount,
+            });
         }
     }
 }
---
+++ FormExtractor/FormExtractor/Models/AzureInvoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormExtractor.Models
+{
+    public class AzureInvoice
+    {
+        public string Id { get; set; }
+
+        public double Total { get; set; }
+
+        public bool Success { get; set; }
+
+        public string JSON { get; set; }
+    }
+}
